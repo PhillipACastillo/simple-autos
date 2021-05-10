c29bec90d5dcdb0678300bbd8e021f67b04fd973
Raf-Batista <RafaelB2424@gmail.com>
2021-05-10 12:49:46 -0400

    Initial commit

---
+++ src/test/java/com/ascent/autosapi/AutosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ascent.autosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutosApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
